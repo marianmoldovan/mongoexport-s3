c1c945788cc4cc03c5df4cc7234347b9b8ca2257
Marian C Moldovan <moldovan0991@gmail.com>
2021-03-17 00:48:41 +0100

    Update export.sh
    Added extra collections

--- export.sh
+++ export.sh
@@ -11,6 +11,22 @@ mongoexport --collection=reviewgenerator --out=reviewgenerator.json --uri=${MONG
 echo "Uploading reviewgenerator file"
 aws s3 cp reviewgenerator.json s3://${S3_BUCKET}/reviewgenerator.json
 echo "Exporting completed for reviewgenerator"
+echo "Exporting texttopay to s3://${S3_BUCKET}/texttopay.json"
+mongoexport --collection=texttopay --out=texttopay.json --uri=${MONGO_HOST}
+echo "Uploading texttopay file"
+aws s3 cp texttopay.json s3://${S3_BUCKET}/texttopay.json
+echo "Exporting telemedicine to s3://${S3_BUCKET}/telemedicine.json"
+mongoexport --collection=telemedicine --out=telemedicine.json --uri=${MONGO_HOST}
+echo "Uploading telemedicine file"
+aws s3 cp telemedicine.json s3://${S3_BUCKET}/telemedicine.json
+echo "Exporting missedcallai to s3://${S3_BUCKET}/missedcallai.json"
+mongoexport --collection=missedcallai --out=missedcallai.json --uri=${MONGO_HOST}
+echo "Uploading missedcallai file"
+aws s3 cp missedcallai.json s3://${S3_BUCKET}/missedcallai.json
+echo "Exporting aiautodialer to s3://${S3_BUCKET}/aiautodialer.json"
+mongoexport --collection=aiautodialer --out=aiautodialer.json --uri=${MONGO_HOST}
+echo "Uploading aiautodialer file"
+aws s3 cp aiautodialer.json s3://${S3_BUCKET}/aiautodialer.json
 echo "Exporting ${MONGO_COLLECTION} to s3://${S3_BUCKET}/${BACKUP_NAME}"
 mongoexport --sort='{start_time: -1}' --collection=${MONGO_COLLECTION} --out=${BACKUP_NAME} --uri=${MONGO_HOST}
 echo "Cleaning file"
